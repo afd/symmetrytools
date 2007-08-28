b8c1d7933e064b0bea690ced79630d5833585ad8
ally <ally@af1217dd-cd28-0410-9574-8a71f6cdaffb>
2007-08-28 12:14:18 +0000

    Added missing Environment file
    git-svn-id: svn+ssh://svnuser.doc.ic.ac.uk/homes/afd/svn/symmetrytools/trunk@14 af1217dd-cd28-0410-9574-8a71f6cdaffb

---
+++ src/etch/env/Environment.java
@@ -0,0 +1,124 @@
+package src.etch.env;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Vector;
+
+import junit.framework.Assert;
+import src.etch.typeinference.Substituter;
+import src.etch.types.Type;
+import src.etch.types.VisibleType;
+
+
+/* An environment is a vector of hash tables, used as a stack.
+ The 'put' method adds an entry to the hash table at the top of the stack
+ (first element of the vector). The 'get' method searches down the stack.
+ The 'openScope' method creates a new hash table on the top of the stack.
+ The 'closeScope' method removes the top-most hash table. */
+
+public class Environment {
+
+	private Vector<Map<String,EnvEntry>> stack;
+
+	public Environment() {
+		stack = new Vector<Map<String,EnvEntry>>(1, 1);
+		stack.addElement(new HashMap<String,EnvEntry>());
+	}
+
+	public void openScope() {
+		stack.addElement(new HashMap<String,EnvEntry>());
+	}
+
+	public void closeScope() {
+		stack.removeElementAt(stack.size() - 1);
+	}
+
+	public EnvEntry putGlobal(String n, EnvEntry e) {
+		// This is necessary when adding proctypes.  It is a bit clumsy, and a better
+		// solution could possibly be found.
+		EnvEntry result = get(n);
+		stack.elementAt(0).put(n, e);
+		return result;
+	}
+
+	public EnvEntry put(String n, EnvEntry e) {
+		return stack.elementAt(stack.size() - 1).put(n, e);
+	}
+
+	public EnvEntry get(String n) {
+		for (int i = stack.size() - 1; i >= 0; i--) {
+			EnvEntry e = (EnvEntry) stack.elementAt(i).get(n);
+			if (e != null)
+				return e;
+		}
+		return null;
+	}
+
+	public EnvEntry getLocal(String n) {
+		// Only searches the current scope
+		return stack.elementAt(stack.size() - 1).get(n);
+
+	}
+
+	public Map<String,EnvEntry> getTopEntries() {
+		return stack.elementAt(stack.size() - 1);
+	}
+	
+	public Map<String,ProctypeEntry> getProctypeEntries() {
+		Map<String,ProctypeEntry> result = new HashMap<String,ProctypeEntry>();
+
+		for(Iterator<String> i = stack.elementAt(0).keySet().iterator(); i.hasNext(); ) {
+			String s = i.next();
+			EnvEntry e = stack.elementAt(0).get(s);
+			if (e instanceof ProctypeEntry)
+				result.put(s, (ProctypeEntry)e);
+		}
+		return result;
+	}
+
+	public Map getChannelEntries() {
+		Map<String,ChannelEntry> result = new HashMap<String,ChannelEntry>();
+
+		for(Iterator<String> i = stack.elementAt(0).keySet().iterator(); i.hasNext(); ) {
+			String s = i.next();
+			EnvEntry e = stack.elementAt(0).get(s);
+			if (e instanceof ChannelEntry)
+				result.put(s, (ChannelEntry)e);
+		}
+		return result;
+	}
+
+	public void applySubsitutions(Substituter substituter) {
+
+		Assert.assertTrue(stack.size()==1);
+
+		for(Iterator<EnvEntry> i = stack.elementAt(0).values().iterator(); i.hasNext();) {
+			EnvEntry e = i.next();
+			if(e instanceof VarEntry) {
+				Type typeAfterSubstitutions = substituter.applySubstitutions(((VarEntry)e).getType());
+				Assert.assertTrue(typeAfterSubstitutions instanceof VisibleType);
+				((VarEntry)e).setType((VisibleType) typeAfterSubstitutions);
+			}
+			
+			if(e instanceof ProctypeEntry) {
+				Map<String,EnvEntry> localScope = ((ProctypeEntry)e).getLocalScope();
+
+				for(Iterator<String> iter = localScope.keySet().iterator(); iter.hasNext();) {
+					String name = iter.next();
+					if(localScope.get(name) instanceof VarEntry) {
+						Type typeAfterSubstitutions = substituter.applySubstitutions(
+								((VarEntry)localScope.get(name)).getType());
+						Assert.assertTrue(typeAfterSubstitutions instanceof VisibleType);
+						((VarEntry)localScope.get(name)).setType((VisibleType) typeAfterSubstitutions);
+					}
+				}
+			}			
+		}
+	}
+
+	public void restoreScope(Map<String, EnvEntry> localScope) {
+		stack.addElement(localScope);
+	}
+
+}
