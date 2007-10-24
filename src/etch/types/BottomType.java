751e6edac32b0021dbd447be5a3ee70a2b0f03f1
ally <ally@af1217dd-cd28-0410-9574-8a71f6cdaffb>
2007-10-24 11:25:45 +0000

    Added class to represent bottom type
    git-svn-id: svn+ssh://svnuser.doc.ic.ac.uk/homes/afd/svn/symmetrytools/trunk@34 af1217dd-cd28-0410-9574-8a71f6cdaffb

---
+++ src/etch/types/BottomType.java
@@ -0,0 +1,23 @@
+package src.etch.types;
+
+import junit.framework.Assert;
+
+public final class BottomType extends SimpleType implements VisibleType {
+
+	private int numberConstructed = 0;
+
+	public static final BottomType uniqueInstance = new BottomType();
+
+	private BottomType() {
+		Assert.assertEquals(numberConstructed,0);
+	}
+	
+	public String name() {
+		return "bottom";
+	}
+
+	public boolean isSubtype(Type t) {
+		return true;
+	}
+
+}
