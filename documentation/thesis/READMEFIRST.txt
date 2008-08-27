6d24b8b11eb642a567c8d40a269c56a7da0661aa
ally <ally@af1217dd-cd28-0410-9574-8a71f6cdaffb>
2008-08-27 09:04:37 +0000

    Updated documentation, and improved the "makerelease" batch file.
    git-svn-id: svn+ssh://svnuser.doc.ic.ac.uk/homes/afd/svn/symmetrytools/trunk@82 af1217dd-cd28-0410-9574-8a71f6cdaffb

---
+++ documentation/thesis/READMEFIRST.txt
@@ -0,0 +1,4 @@
+The Ph.D. thesis contains instructions on downloading and installing TopSPIN
+which were correct when the thesis was submitted.  However, TopSPIN has
+changed since then, so please stick to the instructions contained in the
+TopSPIN user manual.
--- makerelease.bat
+++ makerelease.bat
@@ -1,55 +1,74 @@
+echo off
+
 set VERSION=2.1
 
-rm -rf release
+set RELEASE=TopSPIN_%VERSION%
 
-mkdir release
+make clean
 
-mkdir release\documentation
+make jars
 
-mkdir release\documentation\papers
+rm -rf %RELEASE%
 
-mkdir release\documentation\thesis
+rm -f %RELEASE%.tar.gz
 
-mkdir release\documentation\manual
-cp documentation\manual\manual.pdf release\documentation\manual\TopSPIN_%VERSION%_manual.pdf
+mkdir %RELEASE%
 
-mkdir release\saucy
-cp saucy/LICENSE release/saucy/LICENSE
-cp saucy/Makefile release/saucy/Makefile
-cp saucy/README release/saucy/README
-cp saucy/main.c release/saucy/main.c
-cp saucy/saucy.c release/saucy/saucy.c
-cp saucy/saucy.h release/saucy/saucy.h
-cp saucy/saucyio.c release/saucy/saucyio.c
+mkdir %RELEASE%\documentation
 
-mkdir release\Common
-cp Common\Classify.gap release\Common\Classify.gap
-cp Common\Enumerate.gap release\Common\Enumerate.gap
-cp Common\LocalSearch.gap release\Common\LocalSearch.gap
-cp Common\OutputPerm.gap release\Common\OutputPerm.gap
-cp Common\StabilizerChainCosets.gap release\Common\StabilizerChainCosets.gap
-cp Common\WorkspaceGenerator.gap release\Common\WorkspaceGenerator.gap
-cp Common\Disjoint.gap release\Common\Disjoint.gap
-cp Common\EnumerationStrategies.gap release\Common\EnumerationStrategies.gap
-cp Common\Minimising.gap release\Common\Minimising.gap
-cp Common\PermutationToTranspositions.gap release\Common\PermutationToTranspositions.gap
-cp Common\Verify.gap release\Common\Verify.gap
-cp Common\Wreath.gap release\Common\Wreath.gap
-cp Common\groupBasic.c release\Common\groupBasic.c
-cp Common\groupTranspositions.c release\Common\groupTranspositions.c
-cp Common\parallel_symmetry_cell_ppu.c release\Common\parallel_symmetry_cell_ppu.c
-cp Common\parallel_symmetry_cell_spu.c release\Common\parallel_symmetry_cell_spu.c
-cp Common\parallel_symmetry_pthreads.c release\Common\parallel_symmetry_pthreads.c
-cp Common\groupBasic.h release\Common\groupBasic.h
-cp Common\groupTranspositions.h release\Common\groupTranspositions.h
-cp Common\parallel_symmetry_cell_ppu.h release\Common\parallel_symmetry_cell_ppu.h
-cp Common\parallel_symmetry_cell_spu.h release\Common\parallel_symmetry_cell_spu.h
-cp Common\parallel_symmetry_pthreads.h release\Common\parallel_symmetry_pthreads.h
-cp Common\segment.h release\Common\segment.h
+mkdir %RELEASE%\documentation\papers
+cp documentation\papers\FM2005.pdf %RELEASE%\documentation\papers\FM2005.pdf
+cp documentation\papers\FM2006.pdf %RELEASE%\documentation\papers\FM2006.pdf
+cp documentation\papers\AMAST2006.pdf %RELEASE%\documentation\papers\AMAST2006.pdf
 
-mkdir release\examples
-cp examples\loadbalancer.p release\examples\loadbalancer.p
+mkdir %RELEASE%\documentation\thesis
+cp documentation\thesis\DonaldsonThesis.pdf %RELEASE%\documentation\thesis\DonaldsonThesis.pdf
 
-cp TopSPIN.jar release\TopSPIN_%VERSION%.jar
+mkdir %RELEASE%\documentation\manual
+cp documentation\manual\manual.pdf %RELEASE%\documentation\manual\TopSPIN_%VERSION%_manual.pdf
+
+mkdir %RELEASE%\saucy
+cp saucy/LICENSE %RELEASE%/saucy/LICENSE
+cp saucy/Makefile %RELEASE%/saucy/Makefile
+cp saucy/README %RELEASE%/saucy/README
+cp saucy/main.c %RELEASE%/saucy/main.c
+cp saucy/saucy.c %RELEASE%/saucy/saucy.c
+cp saucy/saucy.h %RELEASE%/saucy/saucy.h
+cp saucy/saucyio.c %RELEASE%/saucy/saucyio.c
+
+mkdir %RELEASE%\Common
+cp Common\Classify.gap %RELEASE%\Common\Classify.gap
+cp Common\Enumerate.gap %RELEASE%\Common\Enumerate.gap
+cp Common\LocalSearch.gap %RELEASE%\Common\LocalSearch.gap
+cp Common\OutputPerm.gap %RELEASE%\Common\OutputPerm.gap
+cp Common\StabilizerChainCosets.gap %RELEASE%\Common\StabilizerChainCosets.gap
+cp Common\WorkspaceGenerator.gap %RELEASE%\Common\WorkspaceGenerator.gap
+cp Common\Disjoint.gap %RELEASE%\Common\Disjoint.gap
+cp Common\EnumerationStrategies.gap %RELEASE%\Common\EnumerationStrategies.gap
+cp Common\Minimising.gap %RELEASE%\Common\Minimising.gap
+cp Common\PermutationToTranspositions.gap %RELEASE%\Common\PermutationToTranspositions.gap
+cp Common\Verify.gap %RELEASE%\Common\Verify.gap
+cp Common\Wreath.gap %RELEASE%\Common\Wreath.gap
+cp Common\groupBasic.c %RELEASE%\Common\groupBasic.c
+cp Common\groupTranspositions.c %RELEASE%\Common\groupTranspositions.c
+cp Common\parallel_symmetry_cell_ppu.c %RELEASE%\Common\parallel_symmetry_cell_ppu.c
+cp Common\parallel_symmetry_cell_spu.c %RELEASE%\Common\parallel_symmetry_cell_spu.c
+cp Common\parallel_symmetry_pthreads.c %RELEASE%\Common\parallel_symmetry_pthreads.c
+cp Common\groupBasic.h %RELEASE%\Common\groupBasic.h
+cp Common\groupTranspositions.h %RELEASE%\Common\groupTranspositions.h
+cp Common\parallel_symmetry_cell_ppu.h %RELEASE%\Common\parallel_symmetry_cell_ppu.h
+cp Common\parallel_symmetry_cell_spu.h %RELEASE%\Common\parallel_symmetry_cell_spu.h
+cp Common\parallel_symmetry_pthreads.h %RELEASE%\Common\parallel_symmetry_pthreads.h
+cp Common\segment.h %RELEASE%\Common\segment.h
+
+mkdir %RELEASE%\examples
+cp examples\loadbalancer.p %RELEASE%\examples\loadbalancer.p
+
+cp TopSPIN.jar %RELEASE%\TopSPIN_%VERSION%.jar
+
+
+tar -cvf %RELEASE%.tar %RELEASE%
+gzip %RELEASE%.tar
 
 pause
+
